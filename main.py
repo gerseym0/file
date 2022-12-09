312de7b5dbe6b2e4da76c3a8285fb715b7aa31e1
gerseym0 <120164264+gerseym0@users.noreply.github.com>
2022-12-09 10:29:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+file = open("text.txt", "rt", encoding = "utf-8")
+text = file.read()
+print("Кількість слів у рядку: " + str(len(text.split())))
+len = 0
+for с in text:
+    if с != ' ':
+        len += 1        
+print("Кількість символів в рядку: " + str(len))
